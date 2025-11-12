d98f736915f6b664051c051ea8e108f1617e696f
Ivan Zhao <ivanzhao8@gmail.com>
2025-11-12 09:16:35 -0800

    adds og image and data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,32 @@ export const metadata: Metadata = {
   title: "Ivan Zhao",
   description:
     "Ivan Zhao is a creative technologist with a love for the weird and wacky things in the world.",
+  openGraph: {
+    title: "Ivan Zhao",
+    description:
+      "Ivan Zhao is a creative technologist with a love for the weird and wacky things in the world.",
+    url: "https://ivanzhao.me",
+    siteName: "Ivan Zhao",
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Ivan Zhao",
+    description:
+      "Ivan Zhao is a creative technologist with a love for the weird and wacky things in the world.",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
 };
 
 export default function RootLayout({
